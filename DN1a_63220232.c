7ace5fc5438c6939c7fd938ab08519ff48c91180
Gasper Novak <125385025+gaspernovak20@users.noreply.github.com>
2023-05-12 23:34:56 +0200

    Add files via upload

---
+++ DN1a_63220232.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include <math.h>
+
+int getInt();   
+int putInt();   
+
+int main(){
+
+    int x = getInt();
+    int y = getInt();
+
+    int z = x + y;
+
+    putInt(z);
+
+    return 0;
+}
+
+int getInt(){
+    bool predznaceno = false;
+    int stevilo = 0;
+
+    int x = getchar();
+
+
+    if(x == '-'){
+        predznaceno = true;
+        x = getchar();
+    }
+
+    while( x != ' ' && x != '\n'){
+        stevilo = (stevilo*10) + ( x - '0' );
+        x = getchar();
+    }
+
+    if(predznaceno){
+        stevilo = stevilo* (-1);
+    }
+
+    return stevilo;
+}
+
+int putInt(int x){
+    int delitelj = 1;
+
+    if( x < 0 ){
+       putchar('-');
+       x = x * (-1);
+    }
+
+    while(( x / delitelj ) > 9){
+        delitelj *=10;
+    }
+
+    while(delitelj > 0){
+
+        putchar((x/delitelj)+ '0');
+
+        x = x % delitelj; 
+
+        
+        delitelj /= 10;
+    }
+
+    
+    return 0;
+}
