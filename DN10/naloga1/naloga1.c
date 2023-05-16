5a5b4916e162d259d8c3d8e3ec4d7ea1e175a3e1
gaspernovak20 <gaspernovak22@gmail.com>
2023-05-16 16:35:48 +0200

    Deluje ustvarjamnje tabelo VO

--- DN10/naloga1/naloga1.c
+++ DN10/naloga1/naloga1.c
@@ -9,7 +9,7 @@
 VO **opravili(Student **studentje, int stStudentov, char *predmet, int *stVO)
 {
 
-    VO **student_ocena = (VO **)malloc(1000 * sizeof(VO *));
+    VO **student_ocena = malloc(1001 * sizeof(*student_ocena));
     if (student_ocena == NULL)
         exit(1);
 
@@ -19,33 +19,33 @@ VO **opravili(Student **studentje, int stStudentov, char *predmet, int *stVO)
     for (int i = 0; i < stStudentov; i++)
     {
         Student *student = studentje[i];
-        printf("%d ", student->vpisna);
 
         // skozi vse predmete studenta
         for (int j = 0; j < student->stPO; j++)
         {
-            printf("%s ", student->po[j].predmet);
 
             // previrjamo ali je pravilen predmet
             if (strcmp(student->po[j].predmet, predmet) == 0)
             {
                 // dodamo VO v tabelo
-                student_ocena[st_VO]->vpisna = student->vpisna;
-                student_ocena[st_VO]->ocena = student->po[j].ocena;
+                VO *new_vo = malloc(sizeof *new_vo);
 
+                new_vo->vpisna = student->vpisna;
+                new_vo->ocena = student->po[j].ocena;
+
+                student_ocena[st_VO] = new_vo;
+                // povecamo velikost "student_ocena" tabele
                 st_VO++;
             }
-            printf("%d  ", st_VO);
         }
-        printf("\n");
     }
 
     *stVO = st_VO;
 
-    printf("konec");
-
     return student_ocena;
 }
+
+#ifndef test
 
 void izvedi(Student **studentje, int stStudentov, char *predmet)
 {
@@ -63,8 +63,6 @@ void izvedi(Student **studentje, int stStudentov, char *predmet)
     }
     printf("]\n");
 }
-
-#ifndef test
 
 int main()
 {
