4726d469c7a046534ddc8ac42823a514e8c05d4e
gaspernovak20 <gaspernovak22@gmail.com>
2023-05-28 11:03:19 +0200

    8/10; test08 test09 timeout

--- K1/K1a/naloga3/naloga3.c
+++ K1/K1a/naloga3/naloga3.c
@@ -13,8 +13,47 @@
 
 // po potrebi dopolnite ...
 
-int main() {
+int kombinacije(int koncna_vsota, int m, int a, int vsota)
+{
+    int kom = 0;
+
+    if (vsota > koncna_vsota)
+    {
+        return 0;
+    }
+    else if (vsota == koncna_vsota)
+    {
+        return 1;
+    }
+    else
+    {
+        for (int b = m; b <= koncna_vsota; b++)
+        {
+
+            if (a < b)
+            {
+                kom += kombinacije(koncna_vsota, m, 1, (vsota + a * b));
+            }
+        }
+        if ((a + 1) < koncna_vsota)
+        {
+            kom += kombinacije(koncna_vsota, m, a + 1, vsota);
+        }
+        return kom;
+    }
+}
+
+int main()
+{
     // dopolnite ...
+
+    int n, m;
+
+    scanf("%d %d", &n, &m);
+
+    int st_kombinacij = kombinacije(n, m, 1, 0);
+
+    printf("%d", st_kombinacij);
 
     return 0;
 }
