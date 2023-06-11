0828b73efd9b2507fb80d95e54f27b6d93a0c72f
gaspernovak20 <gaspernovak22@gmail.com>
2023-06-11 19:27:48 +0200

    deluje

--- K1/K1a/naloga1/naloga1.c
+++ K1/K1a/naloga1/naloga1.c
@@ -4,10 +4,84 @@
 #include <stdlib.h>
 #include <string.h>
 
-// po potrebi dopolnite ...
+//nipotrebno sm previru ce pravilno berem zaporedje
+void izpis(int *zaporedje, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", zaporedje[i]);
+    }
 
-int main() {
+    return;
+}
+
+void polnjenje(int *zaporedje, int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &zaporedje[i]);
+    }
+
+    // izpis(zaporedje, size);
+
+    return;
+}
+
+void kognitivna_vsota(int *zaporedje, int size)
+{
+
+    int kognitivnost = true;
+    int sum = 0;
+
+    //previrjamo kognitivnos ter seštevamo vsoto zaporedja
+    for (int i = 0; i < (size / 2); i++)
+    {
+        if (zaporedje[i] != zaporedje[((size - 1) - i)])
+        {
+            kognitivnost = false;
+        }
+
+        sum += zaporedje[i];
+        sum += zaporedje[((size - 1) - i)];
+    }
+
+    //vsoti zaporedja prištejemo se sredinski člen če je zaporedje liho
+    if (size % 2 != 0)
+    {
+        sum += zaporedje[(size / 2)];
+    }
+
+    // izpis rezultatov
+
+    if (kognitivnost)
+    {
+        printf("DA\n");
+    }
+    else
+    {
+        printf("NE\n");
+    }
+
+    printf("%d", sum);
+
+    return;
+}
+
+int main()
+{
     // dopolnite ...
+
+    int n;
+    scanf("%d", &n);
+
+    int *zaporedje = (int *)malloc(n * sizeof(int));
+
+    polnjenje(zaporedje, n);
+
+    kognitivna_vsota(zaporedje, n);
+
+    free(zaporedje);
 
     return 0;
 }
