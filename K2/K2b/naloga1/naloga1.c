e69585b0e2fd6f8e54cc6ea96eb65da910073bde
gaspernovak20 <gaspernovak22@gmail.com>
2023-06-12 16:55:49 +0200

    naloga 1 deluje

--- K2/K2b/naloga1/naloga1.c
+++ K2/K2b/naloga1/naloga1.c
@@ -6,8 +6,48 @@
 
 // po potrebi dopolnite ...
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     // dopolnite ...
+
+    FILE *inp = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "w");
+
+    char *buffer = malloc(100000 * sizeof(char));
+    size_t index = 0;
+
+    bool nova_vrstica = true;
+    char ch;
+
+    while ((ch = getc(inp)) != EOF)
+    {
+        if (nova_vrstica && ch != ' ')
+        {
+
+            if (ch != '\n')
+            {
+                nova_vrstica = false;
+            }
+
+            buffer[index] = ch;
+            index++;
+        }
+        else if (!nova_vrstica)
+        {
+
+            if (ch == '\n')
+            {
+                nova_vrstica = true;
+            }
+
+            buffer[index] = ch;
+            index++;
+        }
+    }
+
+    fprintf(out, "%s", buffer);
+
+    free(buffer);
 
     return 0;
 }
