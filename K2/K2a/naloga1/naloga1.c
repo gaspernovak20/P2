15e6ce4c883cf4a200ce252e374c56bd4c930aa6
gaspernovak20 <gaspernovak22@gmail.com>
2023-06-12 10:52:37 +0200

    naloga 1 deluje

--- K2/K2a/naloga1/naloga1.c
+++ K2/K2a/naloga1/naloga1.c
@@ -4,10 +4,65 @@
 #include <stdbool.h>
 #include <string.h>
 
-// po potrebi dopolnite ...
+bool contains(char c, char *nadomestni)
+{
+    for (int i = 0; i < 6; i++)
+    {
+        if (c == nadomestni[i])
+        {
+            return true;
+        }
+    }
 
-int main(int argc, char** argv) {
-    // dopolnite ...
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+
+    FILE *inp = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "w");
+
+    char *buffer = malloc(100000 * sizeof(char));
+    char *nadomestni = "CcSsZz";
+
+    size_t index = 0;
+
+    char prev = fgetc(inp);
+    char next;
+
+    bool change = false;
+
+    while ((next = fgetc(inp)) != EOF)
+    {
+        if (!change)
+        {
+            if (prev == '"' && contains(next, nadomestni))
+            {
+                buffer[index] = next;
+                index++;
+                change = true;
+            }
+            else
+            {
+                buffer[index] = prev;
+                index++;
+            }
+        }
+        else
+        {
+            change = false;
+        }
+
+        prev = next;
+    }
+
+    fprintf(out, "%s", buffer);
+    
+    //fwrite(buffer, sizeof(char) , index ,out);
+
+    fclose(inp);
+    fclose(out);
 
     return 0;
 }
